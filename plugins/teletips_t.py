c67824c07d7432ebd033d734a32d584c9e0f37b8
cosmoostart <74989839+cosmoostart@users.noreply.github.com>
2021-10-09 17:24:02 +0500

    Update teletips_t.py

--- plugins/teletips_t.py
+++ plugins/teletips_t.py
@@ -5,7 +5,7 @@
 # Changing the code is not allowed! Read GNU AFFERO GENERAL PUBLIC LICENSE: https://github.com/teletips/CountdownTimer-TeLeTiPs/blob/main/LICENSE
                        
   
-START_TEXT = 'Heya,\nMy name is **Countdown Timer** ⏰\n\nI can countdown to your important events in **any group chat**. 🎉\nUse me to keep track of how much time is left for the event ⏳\n\nHit **❓HELP** to find out more about how to use me.'
+START_TEXT = 'Heya,\nMy name is **Countdown Timer** ⏰\n\nI can countdown to your important events in **any group chat**. 🎉\nPowered by Lzez ⏳\n\nHit **❓HELP** to find out more about how to use me.'
 
 GROUP_TEXT="""
 👥 <u>**Help & Support Groups**</u>
